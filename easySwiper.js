d24eeb62d1dd04b8c35ee5e035c11918945fbee4
HouMingJie <273338979@qq.com>
2016-02-22 15:15:44 +0800

    支持循环滚动

--- easySwiper.js
+++ easySwiper.js
@@ -62,16 +62,19 @@
 
     //默认配置
     var SwiperDefault = {
-        pageClass:"s-page",//默认页面选择符
-        wrapperClass:"s-wrapper",
-        pagination:"false",//默认不显示分页
-        isHorizontal:true,//默认方向 水平
-        threshold:50,//临界值,超过临界值触发翻页
-        duration:"0.5s",//持续时间
-        supportWheel:true,//支持鼠标滚轮翻页
-        wheelThreshold:2,//鼠标滚动触发翻页次数
-        auto:true,//是否自动滚动
-        autoInterval:5000,//自动滚动时间间隔
+        pageClass:      "s-page",          //默认页面类名
+        wrapperClass:   "s-wrapper",       //默认wrapper类名
+        pagination:     "false",           //默认不显示分页
+        isHorizontal:   true,              //默认方向 水平
+        threshold:      50,                //翻页临界值,超过临界值触发翻页
+        duration:       "0.5s",            //持续时间
+        supportWheel:   true,              //支持鼠标滚轮翻页
+        wheelThreshold: 2,                 //鼠标滚动触发翻页所需次数
+        auto:           false,              //是否自动滚动
+        autoInterval:   5000,              //自动滚动时间间隔
+        autoIncreasing: true,              //是否递增自动滚动
+        loop:           true,              //是否循环滚动
+        initPage:       1,                 //初始化页面
     }
 
     function Swiper(target,options){
@@ -84,6 +87,9 @@
         this.canMove =false;
         this.moveDistance = 0;
         this.pageSize;//每一页的宽度或者高度,取决于移动方向
+        this.pageIndex = 1;//页面索引
+        this.autoTimer = null;
+
 
         if(options){
             options.target = target;
@@ -96,6 +102,7 @@
         if(!options.target){
             return ;
         }
+        this.pageIndex = options.initPage;//页面索引
 
         container = (typeof options.target =="string")?dom.querySelectorAll(options.target):options.target;
 
@@ -118,6 +125,16 @@
         wrapper.style.transitionDuration = this.options.duration;
         pages = [].slice.call(wrapper.querySelectorAll("."+options.pageClass));
 
+        var firstPage,lastPage;
+        if(options.loop){//循环滚动
+            firstPage = pages[0].cloneNode(true);
+            lastPage = pages[pages.length-1].cloneNode(true);
+            wrapper.insertBefore(lastPage,pages[0]);
+            wrapper.insertBefore(firstPage,null);
+            pages.unshift(lastPage);
+            pages.push(firstPage);
+        }
+
         addClass( container,"s-container-" + (options.isHorizontal?"horizontal":"vertical") );
         addClass( wrapper,"s-wrapper" );
         addClass( pages,"s-page");
@@ -125,9 +142,8 @@
         this.wrapper = wrapper;
         this.transFormDistance = 0;
         this.pages = pages;
-        this.pageCount = pages.length;
-        this.index = 1;
-        this.autoTimer = null;
+        this.pageCount = pages.length;//页面总数，如果支持循环则包括在首尾添加的重复页面
+
         this.resize();
         var resizeTimer = null;
         window.addEventListener("resize",function(){//窗体大小变化时节流改变wrapper和page的大小
@@ -167,6 +183,16 @@
                 else if(e.type == "touchmove"){
                     self.moveDistance = isHorizontal?(e.touches[0].pageX - startX):(e.touches[0].pageY - startY);
                 }
+
+                if(self.options.loop){//循环
+                    if(self.pageIndex == 0){
+                        self.goto(self.pageCount-2,true);
+                    }
+                    else if(self.pageIndex == (self.pageCount-1)){
+                        self.goto(1,true);
+                    }
+                }
+
                 self.wrapper.style.transform = "translate"+(isHorizontal?"X":"Y")+"("+(self.transFormDistance+self.moveDistance)+"px)";
                 //阻止默认行为，防止因为用户选择了字符后再拖动（浏览器默认搜索选中的字符）造成的位移混乱
                 e.preventDefault();
@@ -179,14 +205,20 @@
                 self.canMove = false;
                 if(Math.abs(self.moveDistance)>=self.options.threshold){
                     if(self.moveDistance>0){
-                        self.goto(self.index-1);
+                        self.goto(self.pageIndex-1);
                     }
                     else{
-                        self.goto(self.index+1);
+                        self.goto(self.pageIndex+1);
                     }
                 }
                 else{
-                    self.goto(self.index);
+                    self.goto(self.pageIndex);
+                }
+
+                if(options.auto){
+                    //重置自动滚动
+                    clearTimeout(self.autoTimer);
+                    autoMove();
                 }
             }
         }
@@ -209,7 +241,7 @@
 
             deltaScroll += e.delta;
             if(Math.abs(deltaScroll)>=options.wheelThreshold){
-                self.goto(self.index+(deltaScroll>0?-1:1));
+                self.goto(self.pageIndex+(deltaScroll>0?-1:1));
                 deltaScroll = 0;
 
                 if(options.auto){
@@ -227,7 +259,7 @@
         }
 
         function autoMove(){
-            self.autoTimer = setInterval(function(){self.goto(++self.index);},options.autoInterval);
+            self.autoTimer = setInterval(function(){self.goto(self.options.autoIncreasing?(++self.pageIndex):(--self.pageIndex));},options.autoInterval);
         }
 
         if(options.auto){
@@ -260,13 +292,27 @@
                 });
             }
             this.pageSize = size;
-            this.goto(this.index,true);
+            this.goto(this.pageIndex,true);
         },
-        goto:function(index,withOutAnimate){
+        goto:function(pageIndex,withOutAnimate){
+            var trueIndex = 2;//真实目标页面索引
             var isHorizontal = this.options.isHorizontal;
             var pageCount = this.pageCount;
-            index = index>0?(index>pageCount?pageCount:index):1;
-            var distance = -(index-1)*this.pageSize;
+            trueIndex = pageIndex+1;
+            if(this.options.loop){//循环的情况
+                if(trueIndex == 0){
+                    this.goto(this.pageCount-2,true);
+                    trueIndex = this.pageIndex;
+                }
+                else if(trueIndex == (this.pageCount+1)){
+                    this.goto(1,true);
+                    trueIndex = this.pageIndex;
+                }
+            }
+            else{//非循环的情况
+                trueIndex = trueIndex>0?(trueIndex>pageCount?pageCount:trueIndex):1;
+            }
+            var distance = -(trueIndex-1)*this.pageSize;
 
             if(withOutAnimate){
                 this.wrapper.style.transitionDuration = "0s";
@@ -281,7 +327,8 @@
             else{
                 this.wrapper.style.transform = "translateY("+distance+"px)"
             }
-            this.index = index;
+            var offsetHeight = this.wrapper.offsetHeight;//在这调用强制重绘
+            this.pageIndex = trueIndex-1;
             this.transFormDistance = distance;
         },
 
